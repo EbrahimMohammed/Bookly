6d317b5f5779e37b91db2f5adb00ef16df6af1d6
EbrahimMohammed <haamaatya@gmail.com>
2025-01-31 00:41:21 +0300

    Made name, description as value objects in apartment

--- Bookly/Bookly.Domain/Apartments/Apartment.cs
+++ Bookly/Bookly.Domain/Apartments/Apartment.cs
@@ -10,8 +10,8 @@ namespace Bookly.Domain.Apartments
     public sealed class Apartment : Entity
     {
         public Apartment(Guid id,
-            string name,
-            string description,
+            Name name,
+            Description description,
             Address address,
             Money price,
             Money cleaningFee,
@@ -27,8 +27,8 @@ namespace Bookly.Domain.Apartments
             Amenities = amenities;
         }
 
-        public string Name { get; private set; }
-        public string Description { get; private set; }
+        public Name Name { get; private set; }
+        public Description Description { get; private set; }
 
         public Address Address { get; private set; }
         public Money Price { get; private set; }
