2b37b095c6ba2fb8c6b06a4491c667adc73389aa
EbrahimMohammed <haamaatya@gmail.com>
2025-02-04 21:38:36 +0300

    Added application layer

---
+++ Bookly/Bookly.Application/DependancyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Bookly.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookly.Application
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependancyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+            return services;
+        }
+    }
+}
